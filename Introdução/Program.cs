ddf991677245a4e0dda9ea152c1dce4dd3dd428f
erikscar <erikscarcela@gmail.com>
2025-03-29 19:44:12 -0300

    Aula Introdutória da Linguagem

---
+++ Introdução/Program.cs
@@ -0,0 +1,27 @@
+﻿//Linguagens de Programação
+//----A Forma com que comunicamos com o computador, ela precisa ser transcrita em formato binario (COMPILAÇÃO).
+
+//Alto e Baixo Nível (C# é Alto Nível)
+//----Quanto maior o nível MELHOR o entendimento
+
+//Linguagem Compilada
+//----Escrever código e ele ser convertido para binário
+
+//Linguagem Interpretada
+//----Não são convertidos, são apenas lidos pela máquina, feita em tempo real como Javascript (tempo de execução)
+
+//Tipagem de Dados
+//----Obrigam a especificar o tipo de dado, menor liberdade e maior otimização
+
+int idade = 18;
+//idade = 18.2; //Erro
+//idade = "18"; //Erro
+//idade = 'a'; //Erro
+
+//Em Javascript, sempre utiliza o mesmo tamanho de memória, MENOS OTIMIZADO
+
+//let idade = 18;
+//idade = 18; // OK
+//idade = 18.2; //OK
+//idade = "18"; //OK
+//idade = 'a'; //OK
