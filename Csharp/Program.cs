971b2260f04da2185471d362288e0bc3ad773e04
erikscar <erikscarcela@gmail.com>
2025-03-30 13:24:21 -0300

    Características do C#

---
+++ Csharp/Program.cs
@@ -0,0 +1,11 @@
+﻿//C# é um linguagem TIPADA, COMPILADA e GERENCIADA, criada em 2001, principal linguagem da Microsoft
+
+//Gerenciado
+//------Significa que sua execução depende de um gerenciador (C# = RUNTIME / CLR / CLR Runtime), gerencia memoria, alocamento de memória, segurança e recursos básicos.
+
+//Compiladores
+//----Compilação Inicial gera um código intermediário (INTERMEDIATE LANGUAGE (IL)) e depois compila, para que todas as linguagens possam ser compiladas em binário pelo mesmo compilador
+
+//Intermediate Language (essa conversão é feita em JUST IN TIME)
+//----Compilações diferentes, mas alocamento, gerenciamento e execução é a mesma, o CLR recebe um código do ambiente da microsoft e compila para o IL (onde o resultado é sempre o mesmo)
+
